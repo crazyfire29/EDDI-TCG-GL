f9a68764e8ea3dcee8381c75d6fca599026c7391
silenc3502 <gcccompil3r@gmail.com>
2024-07-12 23:37:43 +0900

    feat: background swap

--- src/main/renderer/MainRenderer.ts
+++ src/main/renderer/MainRenderer.ts
@@ -4,12 +4,16 @@ import { BattleFieldUnitScene } from '../../battle_field_unit/scene/BattleFieldU
 import { ResourceManager } from '../../resouce_manager/ResourceManager';
 import { NonBackgroundImage } from '../../shape/image/NonBackgroundImage';
 import {BattleFieldUnitRepository} from "../../battle_field_unit/repository/BattleFieldUnitRepository";
+import {BattleFieldBackgroundService} from "../../battle_field/service/BattleFieldBackgroundService";
+import {BattleFieldBackgroundScene} from "../../battle_field/scene/BattleFieldBackgroundScene";
+import {BattleFieldBackgroundRenderer} from "../../battle_field/renderer/BattleFieldBackgroundRenderer";
 
 export class MainRenderer {
     private scene: THREE.Scene;
     private camera: THREE.OrthographicCamera;
     private renderer: THREE.WebGLRenderer;
     private unitRenderer: BattleFieldUnitRenderer;
+    private backgroundRenderer: BattleFieldBackgroundRenderer;
     private background: NonBackgroundImage | null = null;
     private resourceManager: ResourceManager;
     private originalWidth: number;
@@ -47,30 +51,38 @@ export class MainRenderer {
         this.unitRenderer = new BattleFieldUnitRenderer(unitScene, this.resourceManager);
         this.scene.add(unitScene.getScene());
 
-        this.initializeBackground(viewSize, aspect);
+        // this.initializeBackground(viewSize, aspect);
+
+        const backgroundService = BattleFieldBackgroundService.getInstance();
+        const backgroundScene = new BattleFieldBackgroundScene(this.resourceManager);
+        this.backgroundRenderer = new BattleFieldBackgroundRenderer(backgroundService, backgroundScene);
+
+        this.backgroundRenderer.initializeBackground(viewSize * aspect, viewSize, 'resource/background/battle_field.png');
+
+        this.scene.add(backgroundScene.getScene());
     }
 
-    private initializeBackground(viewSize: number, aspect: number): void {
-        const backgroundImagePath = 'resource/background/battle_field.png';
-        const backgroundWidth = viewSize * aspect;
-        const backgroundHeight = viewSize;
-        this.background = new NonBackgroundImage(backgroundWidth, backgroundHeight, backgroundImagePath, 1, 1, new THREE.Vector2(0, 0), undefined, undefined, undefined, undefined, undefined, () => {
-            if (this.background) {
-                console.log('Background texture loaded');
-                this.background.draw(this.scene);
-                this.ensureBackgroundIsBehind();
-            } else {
-                console.log('Background initialization failed');
-            }
-        });
-    }
-
-    private ensureBackgroundIsBehind(): void {
-        if (this.background && this.background.getMesh()) {
-            this.background.getMesh()!.renderOrder = -1;
-            // console.log('Background render order set to -1');
-        }
-    }
+    // private initializeBackground(viewSize: number, aspect: number): void {
+    //     const backgroundImagePath = 'resource/background/battle_field.png';
+    //     const backgroundWidth = viewSize * aspect;
+    //     const backgroundHeight = viewSize;
+    //     this.background = new NonBackgroundImage(backgroundWidth, backgroundHeight, backgroundImagePath, 1, 1, new THREE.Vector2(0, 0), undefined, undefined, undefined, undefined, undefined, () => {
+    //         if (this.background) {
+    //             console.log('Background texture loaded');
+    //             this.background.draw(this.scene);
+    //             this.ensureBackgroundIsBehind();
+    //         } else {
+    //             console.log('Background initialization failed');
+    //         }
+    //     });
+    // }
+    //
+    // private ensureBackgroundIsBehind(): void {
+    //     if (this.background && this.background.getMesh()) {
+    //         this.background.getMesh()!.renderOrder = -1;
+    //         // console.log('Background render order set to -1');
+    //     }
+    // }
 
     public render(): void {
         this.unitRenderer.render(this.renderer, this.camera);
@@ -91,11 +103,13 @@ export class MainRenderer {
         this.camera.updateProjectionMatrix();
         this.renderer.setSize(newWidth, newHeight);
 
-        if (this.background) {
-            const newBackgroundWidth = newHeight * newAspect;
-            const newBackgroundHeight = newHeight;
-            this.background.setScale(newBackgroundWidth / this.background.getWidth(), newBackgroundHeight / this.background.getHeight());
-        }
+        // if (this.background) {
+        //     const newBackgroundWidth = newHeight * newAspect;
+        //     const newBackgroundHeight = newHeight;
+        //     this.background.setScale(newBackgroundWidth / this.background.getWidth(), newBackgroundHeight / this.background.getHeight());
+        // }
+
+        this.backgroundRenderer.resize(newHeight, newAspect);
 
         const scaleX = newWidth / this.originalWidth;
         const scaleY = newHeight / this.originalHeight;
