d08742f5af6619605062714a2764debeadbc1a23
Sanghoon Lee <gcccompil3r@gmail.com>
2024-09-05 17:59:37 +0900

    feat: 임의의 카드 생성 준비 [ETWGL-29]

--- src/battle_field_unit/repository/BattleFieldUnitRepository.ts
+++ src/battle_field_unit/repository/BattleFieldUnitRepository.ts
@@ -3,6 +3,7 @@ import {BattleFieldUnit} from "../entity/BattleFieldUnit";
 export class BattleFieldUnitRepository {
     private static instance: BattleFieldUnitRepository;
 
+    
     private currentFieldUnitList: BattleFieldUnit[] = []
     private gapOfEachUnit: number = 200
 
