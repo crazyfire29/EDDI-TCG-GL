104b6f4142debec3c55cd665a310bb374afdfce9
silenc3502 <gcccompil3r@gmail.com>
2024-12-15 23:35:45 +0900

    init: [이상훈] battle field refactoring 초기화 [ETWGL-55]

---
+++ test/refactor_draw_your_hand/config/webpack.common.js
@@ -0,0 +1,42 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        rectangle_image: "./test/refactor_draw_your_hand/refactor_draw_your_hand.ts"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.json$/,
+                type: 'json'
+            },
+            {
+                test: /\.mp3$/,
+                use: 'file-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+        alias: {
+            '@resource': path.resolve(__dirname, '../../../resource'),
+        },
+        fallback: {
+            "fs": false,
+            "path": false
+        }
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "../../dist/refactor_draw_your_hand"),
+    },
+};
