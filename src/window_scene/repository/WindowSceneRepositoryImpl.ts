e212a1d4f7ef3889ddb52299d57a0cce67d486c8
silenc3502 <gcccompil3r@gmail.com>
2024-12-17 00:02:11 +0900

    feat: [이상훈] 그래픽 요소를 그릴 수 있는 화면(Scene)을 생성 [ETWGL-78]

--- src/window_scene/repository/WindowSceneRepositoryImpl.ts
+++ src/window_scene/repository/WindowSceneRepositoryImpl.ts
@@ -2,17 +2,27 @@ import {WindowScene} from "../entity/WindowScene";
 import {WindowSceneRepository} from "./WindowSceneRepository";
 
 export class WindowSceneRepositoryImpl implements WindowSceneRepository {
-    private sceneMap: Map<string, WindowScene> = new Map();
+    private static instance: WindowSceneRepositoryImpl;
+    private sceneMap: Map<number, WindowScene> = new Map();
 
-    add(scene: WindowScene): void {
-        this.sceneMap.set(scene.id, scene);
+    private constructor() {}
+
+    public static getInstance(): WindowSceneRepositoryImpl {
+        if (!WindowSceneRepositoryImpl.instance) {
+            WindowSceneRepositoryImpl.instance = new WindowSceneRepositoryImpl();
+        }
+        return WindowSceneRepositoryImpl.instance;
     }
 
-    getById(id: string): WindowScene | undefined {
+    add(scene: WindowScene): void {
+        this.sceneMap.set(scene.getId(), scene);
+    }
+
+    getById(id: number): WindowScene | undefined {
         return this.sceneMap.get(id);
     }
 
-    removeById(id: string): void {
+    removeById(id: number): void {
         this.sceneMap.delete(id);
     }
 
