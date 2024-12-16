c64d9ca57b1ffb1b381a4852e1ad7daa0f4c5018
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2024-12-16 14:31:40 +0900

    test: [이재림] My Deck의 카드 페이지 이동 버튼 정의 및 구성 [ETWGL-64]

---
+++ test/draw_my_deck/DeckCardPageMovementButtonConfigList.ts
@@ -0,0 +1,19 @@
+import {DeckCardPageMovementButtonConfig, DeckCardPageMovementButtonType} from "./DeckCardPageMovementButtonType";
+import * as THREE from 'three';
+
+export class DeckCardPageMovementButtonConfigList {
+    public static readonly deckCardPageMovementButtonConfigs: DeckCardPageMovementButtonConfig[] = [
+        {
+            id: 1,
+            type: DeckCardPageMovementButtonType.PREV,
+            imagePath: 'resource/my_deck/card_page_movement_button/prev_button.png',
+            position: new THREE.Vector2(-140, -400)
+        },
+        {
+            id: 2,
+            type: DeckCardPageMovementButtonType.NEXT,
+            imagePath: 'resource/my_deck/card_page_movement_button/next_button.png',
+            position: new THREE.Vector2(110, -400)
+        }
+    ];
+}
