20a0f974bdafab70904d3dced4b614bb839e89ce
silenc3502 <gcccompil3r@gmail.com>
2024-12-21 22:24:21 +0900

    add: [이상훈] 배틀 필드 상의 카드 일러스트 생성 [ETWGL-84]

--- src/battle_field_hand/service/BattleFieldHandServiceImpl.ts
+++ src/battle_field_hand/service/BattleFieldHandServiceImpl.ts
@@ -8,11 +8,17 @@ import {BattleFieldHandCardPositionRepositoryImpl} from "../../battle_field_card
 import {BattleFieldCardPosition} from "../../battle_field_card_position/entity/BattleFieldCardPosition";
 import {Vector2d} from "../../common/math/Vector2d";
 import {BattleFieldHand} from "../entity/BattleFieldHand";
+import {BattleFieldHandMapRepository} from "../repository/BattleFieldHandMapRepository";
+import {BattleFieldHandMapRepositoryImpl} from "../repository/BattleFieldHandMapRepositoryImpl";
+import {getCardById} from "../../card/utility";
+import * as THREE from "three";
+import {BattleFieldCardScene} from "../../battle_field_card_scene/entity/BattleFieldCardScene";
 
 export class BattleFieldHandServiceImpl implements BattleFieldHandService {
     private static instance: BattleFieldHandServiceImpl;
 
     private battleFieldHandRepository: BattleFieldHandRepository;
+    private battleFieldHandMapRepository: BattleFieldHandMapRepository;
     private battleFieldCardSceneRepository: BattleFieldCardSceneRepository;
     private battleFieldHandCardPositionRepository: BattleFieldHandCardPositionRepository;
 
@@ -26,25 +32,48 @@ export class BattleFieldHandServiceImpl implements BattleFieldHandService {
     private readonly CARD_WIDTH: number = 0.06493506493
     private readonly CARD_HEIGHT: number = this.CARD_WIDTH * 1.615
 
-    private constructor(repository: BattleFieldHandRepository,
-                        sceneRepository: BattleFieldCardSceneRepository,
-                        handPositionRepository: BattleFieldHandCardPositionRepository) {
-        this.battleFieldHandRepository = repository;
-        this.battleFieldCardSceneRepository = sceneRepository;
-        this.battleFieldHandCardPositionRepository = handPositionRepository;
+    private constructor() {
+        this.battleFieldHandRepository = BattleFieldHandRepositoryImpl.getInstance();
+        this.battleFieldHandMapRepository = BattleFieldHandMapRepositoryImpl.getInstance();
+        this.battleFieldCardSceneRepository = BattleFieldCardSceneRepositoryImpl.getInstance();
+        this.battleFieldHandCardPositionRepository = BattleFieldHandCardPositionRepositoryImpl.getInstance();
     }
 
-    public static getInstance(repository: BattleFieldHandRepository = BattleFieldHandRepositoryImpl.getInstance(),
-                              sceneRepository: BattleFieldCardSceneRepository = BattleFieldCardSceneRepositoryImpl.getInstance(),
-                              handPositionRepository: BattleFieldHandCardPositionRepository = BattleFieldHandCardPositionRepositoryImpl.getInstance()): BattleFieldHandServiceImpl {
-        if (!BattleFieldHandServiceImpl.instance) {
-            BattleFieldHandServiceImpl.instance = new BattleFieldHandServiceImpl(
-                repository, sceneRepository, handPositionRepository);
+    public static getInstance(): BattleFieldHandServiceImpl {
+        if (!this.instance) {
+            this.instance = new BattleFieldHandServiceImpl();
         }
-        return BattleFieldHandServiceImpl.instance;
+        return this.instance;
     }
 
-    create(cardId: number): BattleFieldHand {
+    async createHand(cardId: number): Promise<THREE.Group> {
+        const cardGroup = new THREE.Group()
+        const card = getCardById(cardId);
+
+        const handPositionCount = this.battleFieldHandCardPositionRepository.count()
+        console.log(`createHand() handPositionCount: ${handPositionCount}`)
+        const handPositionX = (this.HAND_INITIAL_X + handPositionCount * this.GAP_OF_EACH_CARD) * window.innerWidth;
+        const handPositionY = this.HAND_INITIAL_Y * window.innerHeight
+            + (this.CARD_HEIGHT * this.HALF * window.innerWidth);
+        const handPosition = new Vector2d(handPositionX, handPositionY);
+
+        const cardPosition = new BattleFieldCardPosition(handPosition.getX(), handPosition.getY());
+        console.log(`createHand() cardPosition: ${cardPosition}`)
+        this.battleFieldHandCardPositionRepository.save(cardPosition);
+
+        const createdCardScene: BattleFieldCardScene = await this.battleFieldCardSceneRepository.create(cardId, handPosition);
+        const createdCardMesh = createdCardScene.getMesh()
+        console.log(`createHand() createdCardMesh: ${createdCardMesh}`)
+        console.log("Mesh Position:", createdCardMesh.position);
+        console.log("Mesh Scale:", createdCardMesh.scale);
+        console.log("Mesh Material:", createdCardMesh.material);
+        cardGroup.add(createdCardMesh)
+        console.log(`createHand() cardGroup: ${cardGroup}`)
+
+        return cardGroup
+    }
+
+    createBattleFieldFirstDrawHand() {
         const handPositionCount = this.battleFieldHandCardPositionRepository.count()
         const handPositionX = (this.HAND_INITIAL_X + handPositionCount * this.GAP_OF_EACH_CARD) * window.innerWidth;
         const handPositionY = this.HAND_INITIAL_Y * window.innerHeight
@@ -54,7 +83,7 @@ export class BattleFieldHandServiceImpl implements BattleFieldHandService {
         const cardPosition = new BattleFieldCardPosition(handPosition.getX(), handPosition.getY());
         this.battleFieldHandCardPositionRepository.save(cardPosition);
 
-        const cardScene = this.battleFieldCardSceneRepository.create(cardId, handPosition);
+        // const cardScene = this.battleFieldCardSceneRepository.create(cardId, handPosition);
 
         // this.battleFieldHandCardPositionRepository.save()
 
@@ -65,4 +94,26 @@ export class BattleFieldHandServiceImpl implements BattleFieldHandService {
 
         return this.battleFieldHandRepository.save(1, 1, [1]);
     }
+
+    // create(cardId: number): BattleFieldHand {
+    //     const handPositionCount = this.battleFieldHandCardPositionRepository.count()
+    //     const handPositionX = (this.HAND_INITIAL_X + handPositionCount * this.GAP_OF_EACH_CARD) * window.innerWidth;
+    //     const handPositionY = this.HAND_INITIAL_Y * window.innerHeight
+    //         + (this.CARD_HEIGHT * this.HALF * window.innerWidth);
+    //     const handPosition = new Vector2d(handPositionX, handPositionY);
+    //
+    //     const cardPosition = new BattleFieldCardPosition(handPosition.getX(), handPosition.getY());
+    //     this.battleFieldHandCardPositionRepository.save(cardPosition);
+    //
+    //     const cardScene = this.battleFieldCardSceneRepository.create(cardId, handPosition);
+    //
+    //     // this.battleFieldHandCardPositionRepository.save()
+    //
+    //     // BattleFieldCardPosition
+    //     // this.battleFieldCardPositionRepository.save()
+    //     // this.battleFieldCardSceneRepository.create(cardId)
+    //     // this.battleFieldCardRepository.save(cardSceneId, positionId, attributeMarkIdList, status);
+    //
+    //     return this.battleFieldHandRepository.save(1, 1, [1]);
+    // }
 }
