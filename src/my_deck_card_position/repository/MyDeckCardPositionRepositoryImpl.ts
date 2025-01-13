379294a2cdc094bccb20b6ec916feeccc4bad8b1
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2025-01-13 13:14:37 +0900

    add: [이재림] 카드의 position을 deck id와 card id로 관리하는 기능 추가 [ETWGL-116]

--- src/my_deck_card_position/repository/MyDeckCardPositionRepositoryImpl.ts
+++ src/my_deck_card_position/repository/MyDeckCardPositionRepositoryImpl.ts
@@ -6,6 +6,7 @@ export class MyDeckCardPositionRepositoryImpl implements MyDeckCardPositionRepos
     private static instance: MyDeckCardPositionRepositoryImpl;
     private positionMap: Map<number, MyDeckCardPosition>;
     private cardIdToPositionMap: Map<number, number>;
+    private deckToPositionMap: Map<number, Map<number, MyDeckCardPosition>> = new Map(); // dekId: [cardId: position] 형태로 관리
 
     private initialX = - 0.3985;
     private incrementX = 0.167;
@@ -38,6 +39,22 @@ export class MyDeckCardPositionRepositoryImpl implements MyDeckCardPositionRepos
         return position;
     }
 
+    public savePositionInfo(deckId: number, positionInfoMap: Map<number, MyDeckCardPosition>): void {
+        if (!this.deckToPositionMap.has(deckId)){
+            this.deckToPositionMap.set(deckId, new Map());
+        }
+
+        const deckPositionMap = this.deckToPositionMap.get(deckId)!;
+        positionInfoMap.forEach((position, cardId) => {
+            deckPositionMap.set(cardId, position);
+        });
+    }
+
+    public initialPositionMap(): void {
+        this.positionMap.clear();
+        this.cardIdToPositionMap.clear();
+    }
+
     public save(cardId: number, position: MyDeckCardPosition): void {
         this.positionMap.set(position.id, position);
         this.cardIdToPositionMap.set(cardId, position.id);
@@ -57,6 +74,31 @@ export class MyDeckCardPositionRepositoryImpl implements MyDeckCardPositionRepos
             return null;
         }
         return this.positionMap.get(positionId) || null;
+    }
+
+    // 덱 id에 해당되는 모든 position 객체 불러오기
+    public findCardPositionByDeckId(deckId: number): MyDeckCardPosition[] {
+        const cardPositionMap = this.deckToPositionMap.get(deckId);
+        if (!cardPositionMap) return [];
+
+        return Array.from(cardPositionMap.values());
+    }
+
+    // 특정 덱 id에 해당되는 특정 card의 position 불러오기
+    public findPositionByDeckIdAndCardId(deckId: number, cardId: number): MyDeckCardPosition | null {
+        const cardPositionMap = this.deckToPositionMap.get(deckId);
+        if (!cardPositionMap) {
+            console.warn(`[WARN] Deck with ID ${deckId} not found`);
+            return null;
+        }
+
+        const position = cardPositionMap.get(cardId);
+        if (!position) {
+            console.warn(`[WARN] Card Position with Card ID ${cardId} not found in deck ${deckId}`);
+            return null;
+        }
+
+        return position;
     }
 
     deleteById(positionId: number): boolean {
